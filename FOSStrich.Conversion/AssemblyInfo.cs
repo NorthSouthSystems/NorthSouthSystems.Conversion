f34e623a2cfc8b0432a0c1394a8c957d41a611ac
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2023-07-23 13:59:41 -0400

    Moved AssemblyInfo.cs items into the csproj.

--- FOSStrich.Conversion/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("FOSStrich.Conversion.Test")]
